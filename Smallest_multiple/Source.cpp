a567cdd6d252e9c9124fd0e30d5a7e3a301971e1
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-23 16:11:57 +0700

    Success

--- Smallest_multiple/Source.cpp
+++ Smallest_multiple/Source.cpp
@@ -1,27 +1,26 @@
 #include<stdio.h>
 int main()
 {
-	int num1 = 1, num2 = 2 , sum = 0;
+	int num1 = 1, num2 = 1 , sum = 0;
 	while (num1 > 0 )
 	{
 		num2 = 1;
-		while ((num1%num2 ==0 )&&(num2 <=20))
+		while ((num1 % num2 == 0 )&&( num2 <=20))
 		{
-			if (num2 == 0)
+			if (num2 == 20)
 			{
 				sum = num1;
 				break;
 			}
-
-
+			num2++;
+		}
+		num1++;
+		if (sum > 0)
+		{
+			break;
 		}
 
-
-
 	}
-
-
-
-
+	printf_s("%d\n", sum);
 	return 0;
 }
